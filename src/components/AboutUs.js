d4a0b44ebb9b00038e10498e56e99fecfabe30fb
Rebecca Leung <rjleung@uci.edu>
2021-04-25 10:15:52 -0700

    Adding my image and bio to about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import rebeccaImg from '../res/rebecca.jpg';
 
 export default class AboutUs extends React.Component {
     render() {
         return(
             <div className="page">
                 <h1>About Us</h1>
+                <div class="square">
+                    <img src={rebeccaImg} alt="Photo of girl in front of cherry blossom tree" width="40%" align="left" class="rounded-corners"/>
+                    <p>Rebecca Leung is a fourth-year Computer Science major at UC Irvine. She is passionate about saving the environment 
+                        and being a mindful consumer of technology and electronics. In her free time, she enjoys raising her pothos and cooking.</p>
+                </div>
             </div>
         )
     }
