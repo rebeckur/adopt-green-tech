cea4fb2cc2a4b3cd1cf050a07f0b88818a154f29
Rebecca Leung <rjleung@uci.edu>
2021-04-25 10:36:48 -0700

    Adding style changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,22 +9,25 @@ export default class AboutUs extends React.Component {
             <div className="page">
                 <h1>About Us</h1>
                 <div class="square">
-                    <img src={rebeccaImg} alt="Photo of girl in front of cherry blossom tree" width="40%" align="left" class="rounded-corners"/>
+                    <img src={rebeccaImg} alt="Photo of girl in front of cherry blossom tree" width="55%" align="left" class="rounded-corners"/>
                     <p>Rebecca Leung is a fourth-year Computer Science major at UC Irvine. She is passionate about saving the environment 
                         and being a mindful consumer of technology and electronics. In her free time, she enjoys raising her pothos and cooking.</p>
                 </div>
-                <h4>Find me on these platforms!</h4>
-                <div>
-                    <a href="https://github.com/rebeckur" target="_blank" rel="noreferrer noopener" className="link">
-                        <img src={githubLogo} alt="Github octocat logo" width="32vrm" align="left" class="logo"/>
-                        <p>rebeckur</p>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://www.linkedin.com/in/leungrebecca/" target="_blank" rel="noreferrer noopener" className="link">
-                        <img src={linkedinLogo} alt="LinkedIn logo" width="32vrm" align="left" class="logo"/>
-                        <p>Rebecca Leung</p>
-                    </a>
+                <hr />
+                <div class="square">
+                    <h4>Find me on these platforms!</h4>
+                    <div>
+                        <a href="https://github.com/rebeckur" target="_blank" rel="noreferrer noopener" className="link">
+                            <img src={githubLogo} alt="Github octocat logo" width="32vrm" align="left" class="logo"/>
+                            <p>rebeckur</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://www.linkedin.com/in/leungrebecca/" target="_blank" rel="noreferrer noopener" className="link">
+                            <img src={linkedinLogo} alt="LinkedIn logo" width="32vrm" align="left" class="logo"/>
+                            <p>Rebecca Leung</p>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
