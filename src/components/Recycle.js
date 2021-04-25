fa86a0121161e7d2aa24faf4644d46ae2daf2680
Rebecca Leung <rjleung@uci.edu>
2021-04-24 20:58:41 -0700

    Adding content to recycle page

--- src/components/Recycle.js
+++ src/components/Recycle.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import recycleImg from '../res/recycle.png'
 
 export default class Recycle extends React.Component {
     render() {
         return(
             <div className="page">
+                <img src={recycleImg} alt="Recycle graphic curtesy of rawpixel.com" width="100%" height="100%"/>
                 <h1>Recycle</h1>
                 <br />
                 <div id="batteries">
@@ -16,10 +18,37 @@ export default class Recycle extends React.Component {
                 <br />
                 <div id="phones">
                     <h2>Phones</h2>
+                    <h3>Tech Firm Programs</h3>
+                    <ul>
+                        <li>
+                            Trade in a qualifying phone through <a href="https://www.amazon.com/Trade-In/b?ie=UTF8&node=9187220011" target="_blank" rel="noreferrer noopener">Amazon</a> in 
+                            exchange for gift cards. Amazon accepts phones from the following manufacturers:
+                            <ul>
+                                <li>Apple</li>
+                                <li>Google</li>
+                                <li>LG</li>
+                                <li>Motorola</li>
+                                <li>Samsung</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Through Apple's <a href="https://www.apple.com/shop/trade-in" target="_blank" rel="noreferrer noopener">Trade In program</a>, you can give your device a second life 
+                            where it is either recycled for free or passed on to another owner. In return, you have the option to receive an Apple Gift Card or credit towards your next purchase. 
+                            Consumers are able to exchange iPhone, iPads, and Android devices through the Trade In program.
+                        </li>
+                    </ul>
                 </div>
                 <br />
                 <div id="laptops-computers">
                     <h2>Laptops and Computers</h2>
+                    <h3>Tech Firm Programs</h3>
+                    <ul>
+                        <li>
+                            Through Apple's <a href="https://www.apple.com/shop/trade-in" target="_blank" rel="noreferrer noopener">Trade In program</a>, you can give your device a second life 
+                            where it is either recycled for free or passed on to another owner. In return, you have the option to receive an Apple Gift Card or credit towards your next purchase. 
+                            Macs are eligible for credit, however other laptops and computers are not but will still get recycled for free.
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
