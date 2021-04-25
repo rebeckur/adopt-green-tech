d53074a141e06178bc014fc1b97ed87fad574e96
Rebecca Leung <rjleung@uci.edu>
2021-04-25 02:01:21 -0700

    Cleaning up pages

--- src/components/Recycle.js
+++ src/components/Recycle.js
@@ -36,8 +36,14 @@ export default class Recycle extends React.Component {
                                 <li>Samsung</li>
                             </ul>
                         </li>
-                        
+                        <li>Sprint has a <a href="https://secure.sprintbuyback.com/" target="_blank" rel="noreferrer noopener">Buyback</a> program 
+                        where you can sell your old phones to them and get credit applied to your next Sprint bill.</li>
+                        <li><a href="https://www.bestbuy.com/site/services/recycling" target="_blank" rel="noreferrer noopener">BestBuy</a> offers a free recycling program in-stores. 
+                        However, unlike other programs, you do not receive a credit for recycling your phone.</li>
                     </ul>
+                    <h3>Non-Profit Programs</h3>
+                    <p> You can also recycle your phone with Call2Recycle! 
+                        Find a local phone recycle center <a href="https://www.call2recycle.org/locator/" target="_blank" rel="noreferrer noopener">here</a>.</p>
                 </div>
                 <br />
                 <div id="laptops-computers">
@@ -49,6 +55,8 @@ export default class Recycle extends React.Component {
                             where it is either recycled for free or passed on to another owner. In return, you have the option to receive an Apple Gift Card or credit towards your next purchase. 
                             Macs are eligible to be exchanged for Apple credit, however other laptops and computers are not but will still get recycled for free.
                         </li>
+                        <li><a href="https://www.bestbuy.com/site/services/recycling" target="_blank" rel="noreferrer noopener">BestBuy</a> offers a recycling program in-stores for laptops, computers, and peripherals. 
+                        Depending on the item, there may be a cost for recycling, such as recycling monitors for $29.99.</li>
                     </ul>
                 </div>
             </div>
--- src/components/Reuse.js
+++ src/components/Reuse.js
@@ -21,6 +21,16 @@ export default class Reuse extends React.Component {
                         keyboard since these peripherals can be used for both PCs and Macs.
                     </li>
                     <li>Donate or sell your old but functional devices.</li>
+                    <ul>
+                        <li>
+                            The <a href="https://worldcomputerexchange.org/" target="_blank" rel="noreferrer noopener">World Computer Exchange</a> is an organization
+                            working to diminish the digital divide in developing countries and promote the reuse of electronics.
+                        </li>
+                        <li><a href="https://charity.ebay.com/" target="_blank" rel="noreferrer noopener">eBay for Charity</a> allows you to sell your used devices and donate some or all of the proceeds to a charity of your choice.</li>
+                        <li>
+                            Dell Reconnect is a partnership with Goodwill, and you can donate your devices to any Goodwill location.
+                        </li>
+                    </ul>
                 </ul>
             </div>
         )
