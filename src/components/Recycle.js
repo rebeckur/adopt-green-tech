130b3a23b4125bd3cbf297c4f9ef7f4b5293dfba
Rebecca Leung <rjleung@uci.edu>
2021-04-25 01:42:23 -0700

    Adding content to reuse page

--- src/components/Recycle.js
+++ src/components/Recycle.js
@@ -11,9 +11,9 @@ export default class Recycle extends React.Component {
                 <div id="batteries">
                     <h2>Batteries</h2>
                     <p> You can recycle your old single-use and rechargeable batteries! 
-                        Find a local battery recycle center with <a href="https://www.call2recycle.org/locator/">Call2Recycle</a>.</p>
+                        Find a local battery recycle center with <a href="https://www.call2recycle.org/locator/" target="_blank" rel="noreferrer noopener">Call2Recycle</a>.</p>
                     <p>In addition, some states have special recycling laws -- be sure to double check with your local requirements before disposing your batteries. 
-                        Call2Recycle also has a <a href="https://www.call2recycle.org/recycling-laws-by-state/">resource</a> for learning more about each state's recycling laws.</p>
+                        Call2Recycle also has a <a href="https://www.call2recycle.org/recycling-laws-by-state/" target="_blank" rel="noreferrer noopener">resource</a> for learning more about each state's recycling laws.</p>
                 </div>
                 <br />
                 <div id="phones">
--- src/components/Reuse.js
+++ src/components/Reuse.js
@@ -7,6 +7,21 @@ export default class Reuse extends React.Component {
             <div className="page">
                 <img src={reuseImg} alt="Reuse typography graphic curtesy of rawpixel.com" width="100%" height="100%"/>
                 <h1>Reuse</h1>
+
+                <p>There are many ways you can reuse techonology to be sustainble!</p>
+                <p>Here are some quick tips:</p>
+                <ul>
+                    <li>Consider purchasing refurbished devices. Many times, refurbished devices have had previous owners
+                        and are fixed to work as if it was brand new. Plus, you can also save some money by purchasing refurbished devices!
+                    </li>
+                    <li>When possible, replace the faulty parts of the device and reuse parts you already have.</li>
+                    <li>When purchasing peripherals such as monitors, mice, and keyboards, consider purchasing ones that could
+                        be used by many different devices. For mice and keyboards, I personally recommend getting a <a href="https://www.logitech.com/en-us/products/mice/mx-master-3.910-005620.html" 
+                        target="_blank" rel="noreferrer noopener">Logitech MX Master</a> mouse and a <a href="https://www.keychron.com/" target="_blank" rel="noreferrer noopener">Keychron</a> mechanical 
+                        keyboard since these peripherals can be used for both PCs and Macs.
+                    </li>
+                    <li>Donate or sell your old but functional devices.</li>
+                </ul>
             </div>
         )
     }
