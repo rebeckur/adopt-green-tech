53e888a4a14089425f150938a3d872ae9dbf40f6
Rebecca Leung <rjleung@uci.edu>
2021-04-25 10:29:54 -0700

    Adding links to github and linkedin

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import rebeccaImg from '../res/rebecca.jpg';
+import githubLogo from '../res/GitHub-Mark/PNG/GitHub-Mark-64px.png';
+import linkedinLogo from '../res/LinkedIn-Logos/LI-In-Bug.png';
 
 export default class AboutUs extends React.Component {
     render() {
@@ -10,6 +12,19 @@ export default class AboutUs extends React.Component {
                     <img src={rebeccaImg} alt="Photo of girl in front of cherry blossom tree" width="40%" align="left" class="rounded-corners"/>
                     <p>Rebecca Leung is a fourth-year Computer Science major at UC Irvine. She is passionate about saving the environment 
                         and being a mindful consumer of technology and electronics. In her free time, she enjoys raising her pothos and cooking.</p>
+                </div>
+                <h4>Find me on these platforms!</h4>
+                <div>
+                    <a href="https://github.com/rebeckur" target="_blank" rel="noreferrer noopener" className="link">
+                        <img src={githubLogo} alt="Github octocat logo" width="32vrm" align="left" class="logo"/>
+                        <p>rebeckur</p>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/leungrebecca/" target="_blank" rel="noreferrer noopener" className="link">
+                        <img src={linkedinLogo} alt="LinkedIn logo" width="32vrm" align="left" class="logo"/>
+                        <p>Rebecca Leung</p>
+                    </a>
                 </div>
             </div>
         )
